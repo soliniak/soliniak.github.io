fef90c8fe106fc73f868d358a28dd0d3a6d50e91
Rafi <soliniak@gmail.com>
2018-08-11 11:30:22 +0200

    Passive listeners to improve scrolling

--- examples/yoga/script.js
+++ examples/yoga/script.js
@@ -1,5 +1,19 @@
 'use strict';
 
+// Test via a getter in the options object to see if the passive property is accessed
+var supportsPassive = false;
+try {
+  var opts = Object.defineProperty({}, 'passive', {
+    get: function() {
+      supportsPassive = true;
+    }
+  });
+  window.addEventListener("testPassive", null, opts);
+  window.removeEventListener("testPassive", null, opts);
+} catch (e) {}
+
+// Use our detect's results. passive applied if supported, capture will be false either way.
+// elem.addEventListener('touchstart', fn, supportsPassive ? { passive: true } : false); 
 
 // ---------------------------------------- TOP BAR FIXED START -------------------------------------------------- //
 
@@ -103,7 +117,7 @@ function spin(to){
 
 const html = document.querySelector('html');
 
-html.addEventListener('wheel', findScrollDirectionOtherBrowsers);
+html.addEventListener('wheel', findScrollDirectionOtherBrowsers, supportsPassive ? { passive: true } : false);
 let delta;
 
 function findScrollDirectionOtherBrowsers(event){
@@ -128,7 +142,7 @@ html.addEventListener("touchmove", ()=>{
         menu.style.marginTop = 0;
 	}
 	z = window.scrollY;
-})
+}, supportsPassive ? { passive: true } : false)
 
 // ------------------------------------- BTN TOGGLE MENU -----------------------------
 
