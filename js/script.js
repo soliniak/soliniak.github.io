17cc7e9794f30b9c56f61ccacf1aa78897dfdb5e
Rafi <soliniakdlagier@gmail.com>
2018-11-20 09:03:23 +0100

    Fixed margin at card__text

--- js/script.js
+++ js/script.js
@@ -1,6 +1,132 @@
 "use strict";
-
-import Projects from "/js/projects.js";
+const Projects = {
+  yoga: {
+    name: "yoga",
+    live: "https://soliniak.github.io/Yoga/",
+    src: "https://github.com/soliniak/Yoga",
+    img: "img/project_icons/yoga.jpg",
+    webp: "img/project_icons/yoga.webp",
+    alt: "WWW - WebDev Challenge #16",
+    order: "10",
+    stack: {
+      html5: ["true", '<i class="fab fa-html5 icon--stack"></i>'],
+      js: ["true", '<i class="fab fa-js icon--stack"></i>'],
+      gulp: ["true", '<i class="fab fa-gulp icon--stack"></i>'],
+      photoshop: ["true", '<i class="ps-icon icon--stack"></i>'],
+      npm: ["true", '<i class="fab fa-npm icon--stack"></i>'],
+      sass: ["true", '<i class="fab fa-sass icon--stack"></i>'],
+      react: ["false", '<i class="fab fa-react icon--stack"></i>']
+    }
+  },
+  zest: {
+    name: "zest",
+    live: "https://soliniak.github.io/ZEST/dist/",
+    src: "https://github.com/soliniak/ZEST",
+    img: "img/project_icons/zest.jpg",
+    webp: "img/project_icons/zest.webp",
+    alt: "WWW - WebDev Challenge #12",
+    order: "20",
+    stack: {
+      html5: ["true", '<i class="fab fa-html5 icon--stack"></i>'],
+      js: ["true", '<i class="fab fa-js icon--stack"></i>'],
+      gulp: ["true", '<i class="fab fa-gulp icon--stack"></i>'],
+      photoshop: ["true", '<i class="ps-icon icon--stack"></i>'],
+      npm: ["true", '<i class="fab fa-npm icon--stack"></i>'],
+      sass: ["true", '<i class="fab fa-sass icon--stack"></i>'],
+      react: ["false", '<i class="fab fa-react icon--stack"></i>']
+    }
+  },
+  mogo: {
+    name: "mogo",
+    live: "https://soliniak.github.io/MoGo/",
+    src: "https://github.com/soliniak/mogo",
+    img: "img/project_icons/mogo.jpg",
+    webp: "img/project_icons/mogo.webp",
+    alt: "WWW - WebDev Challenge #35",
+    order: "30",
+    stack: {
+      html5: ["true", '<i class="fab fa-html5 icon--stack"></i>'],
+      js: ["true", '<i class="fab fa-js icon--stack"></i>'],
+      gulp: ["true", '<i class="fab fa-gulp icon--stack"></i>'],
+      photoshop: ["true", '<i class="ps-icon icon--stack"></i>'],
+      npm: ["true", '<i class="fab fa-npm icon--stack"></i>'],
+      sass: ["true", '<i class="fab fa-sass icon--stack"></i>'],
+      react: ["false", '<i class="fab fa-react icon--stack"></i>']
+    }
+  },
+  lorem: {
+    name: "lorem",
+    live: "https://soliniak.github.io/zapakujloremipsum/",
+    src: "https://github.com/soliniak/zapakujloremipsum",
+    img: "img/project_icons/zapakujlorem.jpg",
+    webp: "img/project_icons/zapakujlorem.webp",
+    alt: "WWW - Landing Page",
+    order: "40",
+    stack: {
+      html5: ["true", '<i class="fab fa-html5 icon--stack"></i>'],
+      js: ["true", '<i class="fab fa-js icon--stack"></i>'],
+      gulp: ["true", '<i class="fab fa-gulp icon--stack"></i>'],
+      photoshop: ["false", '<i class="ps-icon icon--stack"></i>'],
+      npm: ["true", '<i class="fab fa-npm icon--stack"></i>'],
+      sass: ["true", '<i class="fab fa-sass icon--stack"></i>'],
+      react: ["false", '<i class="fab fa-react icon--stack"></i>']
+    }
+  },
+  typer: {
+    name: "typer",
+    live: "https://soliniak.github.io/Word-typing-counter/",
+    src: "https://github.com/soliniak/Word-typing-counter",
+    img: "img/project_icons/typer.jpg",
+    webp: "img/project_icons/typer.webp",
+    alt: "APP - word typing counter",
+    order: "25",
+    stack: {
+      html5: ["true", '<i class="fab fa-html5 icon--stack"></i>'],
+      js: ["true", '<i class="fab fa-js icon--stack"></i>'],
+      gulp: ["true", '<i class="fab fa-gulp icon--stack"></i>'],
+      photoshop: ["false", '<i class="ps-icon icon--stack"></i>'],
+      npm: ["true", '<i class="fab fa-npm icon--stack"></i>'],
+      sass: ["true", '<i class="fab fa-sass icon--stack"></i>'],
+      react: ["false", '<i class="fab fa-react icon--stack"></i>']
+    }
+  },
+  weatherApp: {
+    name: "weather",
+    live: "https://soliniak.github.io/Weather-App/",
+    src: "https://github.com/soliniak/Weather-App",
+    img: "img/project_icons/weather-app.jpg",
+    webp: "img/project_icons/weather-app.webp",
+    alt: "APP - REST API Weather App",
+    order: "1",
+    stack: {
+      html5: ["true", '<i class="fab fa-html5 icon--stack"></i>'],
+      js: ["true", '<i class="fab fa-js icon--stack"></i>'],
+      gulp: ["true", '<i class="fab fa-gulp icon--stack"></i>'],
+      photoshop: ["true", '<i class="ps-icon icon--stack"></i>'],
+      npm: ["true", '<i class="fab fa-npm icon--stack"></i>'],
+      sass: ["true", '<i class="fab fa-sass icon--stack"></i>'],
+      react: ["true", '<i class="fab fa-react icon--stack"></i>']
+    }
+  },
+  tictactoe: {
+    name: "TicTacToe",
+    live: "https://s.codepen.io/Rafi-R/debug/NjOwGV/YvAgOnRKbPYA",
+    src: "https://codepen.io/Rafi-R/pen/NjOwGV",
+    img: "img/project_icons/tictactoe.jpg",
+    webp: "img/project_icons/tictactoe.webp",
+    alt: "Game - simple and funny",
+    order: "50",
+    stack: {
+      html5: ["true", '<i class="fab fa-html5 icon--stack"></i>'],
+      js: ["true", '<i class="fab fa-js icon--stack"></i>'],
+      gulp: ["false", '<i class="fab fa-gulp icon--stack"></i>'],
+      photoshop: ["false", '<i class="ps-icon icon--stack"></i>'],
+      npm: ["false", '<i class="fab fa-npm icon--stack"></i>'],
+      sass: ["true", '<i class="fab fa-sass icon--stack"></i>'],
+      react: ["false", '<i class="fab fa-react icon--stack"></i>']
+    }
+  }
+};
 
 // get data from projects and put it in article mockup
 Object.entries(Projects).forEach(([project, projectValue]) => {
